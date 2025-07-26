31b630a8dcb5334f9245e4170017c2f7d908f855
rootikigai <ifykel360@gmail.com>
2025-07-26 19:57:44 +0100

    set the address fields to private following encapsulation principles and corrected the getters as well.

--- src/address.py
+++ src/address.py
@@ -1,15 +1,15 @@
 class Address(object):
     def __init__(self, house_number, street, lga, state):
-        self.house_number = house_number
-        self.street = street
-        self.lga = lga
-        self.state = state
+        self._house_number = house_number
+        self._street = street
+        self._lga = lga
+        self._state = state
 
     def get_house_number(self):
-        return self.house_number
+        return self._house_number
     def get_street(self):
-        return self.street
+        return self._street
     def get_lga(self):
-        return self.lga
+        return self._lga
     def get_state(self):
-        return self.state
+        return self._state
