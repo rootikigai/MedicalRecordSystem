9e96fb17ae456d1c15049ba5d6a223e5affa70a6
rootikigai <ifykel360@gmail.com>
2025-07-26 01:18:39 +0100

    Wrote test to check that address can be created with appropriate fields...house_number, street, lga, state

--- test/address_test.py
+++ test/address_test.py
@@ -1,9 +1,22 @@
 import unittest
 
+from address import Address
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class AddressTest(unittest.TestCase):
+    def test_address_can_be_created(self):
+        house_number = "312"
+        street = "Herbert Macauley Road"
+        lga = "Yaba"
+        state = "Lagos"
+
+        address = Address(house_number, street, lga, state)
+
+        self.assertEqual(address.get_house_number(), house_number)
+        self.assertEqual(address.get_street(), street)
+        self.assertEqual(address.get_lga(), lga)
+        self.assertEqual(address.get_state(), state)
+
 
 
 if __name__ == '__main__':
