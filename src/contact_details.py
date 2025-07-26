df0598d4dc33b19e06625c2dcd95e4d9978b818c
jchinedu <jchinedu080@gmail.com>
2025-07-26 02:51:57 +0100

    Add contact details feature

---
+++ src/contact_details.py
@@ -0,0 +1,40 @@
+import re
+
+class InvalidEmailException(Exception): pass
+class InvalidPhoneException(Exception): pass
+
+class ContactDetails:
+    def __init__(self, name: str, email: str, phone_number: str, address):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise InvalidEmailException("Invalid email format")
+
+        if not re.match(r"^\+?\d{10,15}$", phone_number):
+            raise InvalidPhoneException("Invalid phone number")
+
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.address = address
+
+    def get_name(self):
+        return self.name
+
+    def get_email(self):
+        return self.email
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_address(self):
+        return self.address
+
+    def get_info(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "address": self.address
+        }
+
+    def __str__(self):
+        return f"{self.name}, {self.email}, {self.phone_number}, {self.address}"
--- test/contact_details_test.py
+++ test/contact_details_test.py
@@ -1,8 +1,56 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+from MedicalRecordSystem.src.contact_details import *
+
+
+class TestContactDetails(unittest.TestCase):
+
+    def test_valid_contact_creation(self):
+        contact = ContactDetails(
+            name="John Chinedu",
+            email="john.chinedu@yahoo.com",
+            phone_number="+2348161247818",
+            address="312 hebert macauly way sabo yaba"
+        )
+
+        self.assertEqual(contact.get_name(), "John Chinedu")
+        self.assertEqual(contact.get_email(), "john.chinedu@yahoo.com")
+        self.assertEqual(contact.get_phone_number(), "+2348161247818")
+        self.assertEqual(contact.get_address(), "312 hebert macauly way sabo yaba")
+
+    def test_invalid_email_raises_exception(self):
+        with self.assertRaises(InvalidEmailException):
+            ContactDetails(
+                name="James",
+                email="james#yahoo.com",
+                phone_number="+23446591103",
+                address="456 kaduna street"
+            )
+
+    def test_invalid_phone_raises_exception(self):
+        with self.assertRaises(InvalidPhoneException):
+            ContactDetails(
+                name="IKIGAI",
+                email="IKIGAI@yahoo.com",
+                phone_number="123-abc-7890",
+                address="789 lagos Street"
+            )
+
+    def test_get_info_returns_correct_dictionary(self):
+        contact = ContactDetails(
+            name="john Chinedu",
+            email="john.chinedu@yahoo.com",
+            phone_number="2348161247818",
+            address="312 jakande island"
+        )
+        info = contact.get_info()
+        expected = {
+            "name": "john Chinedu",
+            "email": "john.chinedu@yahoo.com",
+            "phone_number": "2348161247818",
+            "address": "312 jakande island"
+        }
+        self.assertEqual(info, expected)
+
+
